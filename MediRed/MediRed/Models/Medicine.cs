7aabdc54afc3bf6249551453e584654904417aca
EvelynMC <EvelynMC@users.noreply.github.com>
2017-12-04 23:31:05 -0300

    Mantenedor medicine

---
+++ MediRed/MediRed/Models/Medicine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MediRed.Models
+{
+    public class Medicine
+    {
+        public int MedicineId { get; set; }
+        public string Name { get; set; }
+        public string Concentracion { get; set; }
+        public string Description { get; set; }
+    }
+}
