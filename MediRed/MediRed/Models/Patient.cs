58d47cb7f22485ca1048bc63b4c84442830445e3
JP-GITHUB <jose.luis.p10@hotmail.com>
2017-12-09 00:17:50 -0300

    Mantenedor de Pacientes - Herencia con Abstract Class

--- MediRed/MediRed/Models/Patient.cs
+++ MediRed/MediRed/Models/Patient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,16 @@ namespace MediRed.Models
 {
     public class Patient: Person
     {
-        public Diagnostic Diagnostic { get; set; }
+        //public Diagnostic Diagnostic { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Campo no puede superar los 100 caracteres.")]
+        [Display(Name = "Diagnostico")]
+        public string Diagnostic { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Campo no puede superar los 50 caracteres.")]
+        [Display(Name = "Tratatamiento")]
         public string Treatment { get; set; }
     }
 }
--- MediRed/MediRed/Models/Person.cs
+++ MediRed/MediRed/Models/Person.cs
@@ -1,14 +1,38 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MediRed.Models
 {
-    public class Person
+    public abstract class Person
     {
         [Key]
-        public string PersonId { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PersonId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Campo no puede superar los 50 caracteres.")]
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Campo no puede superar los 50 caracteres.")]
+        [Display(Name="Apellidos")]
         public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Campo no puede superar los 50 caracteres.")]
         public string Email { get; set; }
+
+        [Display(Name="Teléfono")]
         public int Phone{ get; set; }
+
+        [Display(Name = "Nombre Completo")]
+        public string FullName
+        {
+            get
+            {
+                return Name + " " + LastName;
+            }
+        }
     }
 }
