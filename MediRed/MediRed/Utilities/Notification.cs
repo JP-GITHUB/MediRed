03df07faa106d80ceff70d97fc234f15dfaab6b1
Instinct <maxcorreoimo@gmail.com>
2017-12-20 20:54:38 -0300

    Done Clase de envio de mensajes
    Se agrego una carpeta Utilities donde se guardan clases utilitarias, especialmente se creo la clase de envio de mensajes. quedara comentado la implementacion del metodo.

---
+++ MediRed/MediRed/Utilities/Notification.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace MediRed.Utilities
+{
+    public class Notification
+    {
+        SmtpClient server = new SmtpClient("smtp.gmail.com", 587);
+
+        public Notification()
+        {
+            server.Credentials = new System.Net.NetworkCredential("MediredMsg@gmail.com", "MediredMs");
+            server.EnableSsl = true;
+        }
+
+        public void MandarCorreo(MailMessage mensaje)
+        {
+            server.Send(mensaje);
+        }
+    }
+
+    //using System.Net.Mail;
+    //
+    //try
+    //        {
+    //            MediRed.Utilities.Notification nf = new MediRed.Utilities.Notification();
+    //MailMessage mnsj = new MailMessage();
+
+    //mnsj.Subject = "Hola Mundo";
+
+    //            mnsj.To.Add(new MailAddress("ecmcaceres@gmail.com"));
+
+    //            mnsj.From = new MailAddress("MediredMsg@gmail.com", "Equipo Medired");
+
+    //mnsj.Body = "  Mensaje de Prueba \n\n Enviado desde C#\n\n TRABAJA";
+
+    //            /* Enviar */
+    //            nf.MandarCorreo(mnsj);
+
+    //            Console.WriteLine("Mensaje enviado correctamente");
+    //        }
+    //        catch (Exception)
+    //        {
+    //            Console.WriteLine("Mensaje No Enviado ");
+    //        }
+}
---
+++ MediRed/TestConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                MediRed.Utilities.Notification nf = new MediRed.Utilities.Notification();
+                MailMessage mnsj = new MailMessage();
+
+                mnsj.Subject = "Hola Mundo";
+
+                mnsj.To.Add(new MailAddress("ecmcaceres@gmail.com"));
+
+                mnsj.From = new MailAddress("MediredMsg@gmail.com", "Equipo Medired");
+
+                mnsj.Body = "  Mensaje de Prueba \n\n Enviado desde C#\n\n TRABAJA";
+
+                /* Enviar */
+                nf.MandarCorreo(mnsj);
+
+                Console.WriteLine("Mensaje enviado correctamente");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Mensaje No Enviado ");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
