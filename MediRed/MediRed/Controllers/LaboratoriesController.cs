cd44070551958731666073f5d0b42cd2b9debe87
EvelynMC <EvelynMC@users.noreply.github.com>
2017-12-23 18:31:22 -0300

    controladores

---
+++ MediRed/MediRed/Controllers/LaboratoriesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MediRed.Context;
+using MediRed.Models;
+
+namespace MediRed.Controllers
+{
+    public class LaboratoriesController : Controller
+    {
+        private MediRedContext db = new MediRedContext();
+
+        // GET: Laboratories
+        public ActionResult Index()
+        {
+            return View(db.Laboratories.ToList());
+        }
+
+        // GET: Laboratories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laboratory laboratory = db.Laboratories.Find(id);
+            if (laboratory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laboratory);
+        }
+
+        // GET: Laboratories/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Laboratories/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LaboratoryId,Name,Description")] Laboratory laboratory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Laboratories.Add(laboratory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(laboratory);
+        }
+
+        // GET: Laboratories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laboratory laboratory = db.Laboratories.Find(id);
+            if (laboratory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laboratory);
+        }
+
+        // POST: Laboratories/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LaboratoryId,Name,Description")] Laboratory laboratory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(laboratory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(laboratory);
+        }
+
+        // GET: Laboratories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laboratory laboratory = db.Laboratories.Find(id);
+            if (laboratory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laboratory);
+        }
+
+        // POST: Laboratories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Laboratory laboratory = db.Laboratories.Find(id);
+            db.Laboratories.Remove(laboratory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
