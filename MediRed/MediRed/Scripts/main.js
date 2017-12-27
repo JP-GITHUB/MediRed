a6cd756dd946cb157d4b365d8bafe8c7c467e708
JP-GITHUB <jose.luis.p10@hotmail.com>
2017-12-27 01:46:35 -0300

    Validación de ruts por js

---
+++ MediRed/MediRed/Scripts/main.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $(".validate-rut").change(function () {
+        $(this).val($(this).val().replace(/[^a-zA-Z 0-9-]+/g, ''));
+
+        if ($(this).val() != "") {
+            if (!checkRut($(this).val())) {
+                $("input[type=submit]").attr("disabled", true);
+                $(".validation-summary-valid ul").append("<li class='li-invalid-rut'>" + "El rut ingresado es inválido" + "</li>");
+                $(".msg-validate-rut").html("* El rut ingresado es inválido");
+            } else {
+                $("input[type=submit]").attr("disabled", false);
+                $(".msg-validate-rut").html("");
+                $(".li-invalid-rut").remove();
+            }
+        } else {
+            $("input[type=submit]").attr("disabled", false);
+            $(".msg-validate-rut").html("");
+            $(".li-invalid-rut").remove();
+        }
+    });
+});
